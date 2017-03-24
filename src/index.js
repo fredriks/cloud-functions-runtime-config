c67e1b422c4a8a9fb49a36ffc47bfb4054025ad0
Fredrik Simón <fredrik@fsimon.net>
2017-03-24 11:28:44 +0100

    Initial import

---
+++ src/index.js
@@ -0,0 +1,68 @@
+const google = require('googleapis');
+const runtimeConfig = google.runtimeconfig('v1beta1');
+
+module.exports = {
+    getVariable: getVariable,
+};
+
+/**
+ * runtimeConfig.getVariable
+ *
+ * @desc Reads a runtime config value
+ *
+ * @param {string} configName The config name of the variable to return
+ * @param {string} variableName The variable name of the variable to return
+ * @return {Promise} Promise that resolves the variable value
+ */
+function getVariable(configName, variableName) {
+    return new Promise(function(resolve, reject) {
+        auth().then(function(authClient) {
+            const projectId = process.env.GCLOUD_PROJECT;
+            const fullyQualifiedName = 'projects/' + projectId
+                    + '/configs/' + configName
+                    + '/variables/' + variableName;
+
+            runtimeConfig.projects.configs.variables.get({
+                auth: authClient,
+                name: fullyQualifiedName,
+            }, function(err, variable) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                resolve(Buffer.from(variable.value, 'base64').toString());
+            });
+        });
+    });
+}
+
+/**
+ * auth
+ *
+ * @desc Authenticates using default credentials
+ *
+ * @return {Promise} Promise that resolves an authClient
+ */
+function auth() {
+    return new Promise(function(resolve, reject) {
+        google.auth.getApplicationDefault(function(err, authClient, projectId) {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            if (authClient.createScopedRequired
+                && authClient.createScopedRequired()
+               ) {
+                const scopes = [
+                    'https://www.googleapis.com/auth/cloud-platform',
+                    'https://www.googleapis.com/auth/cloudruntimeconfig',
+                ];
+                authClient = authClient.createScoped(scopes);
+            }
+
+            resolve(authClient);
+        });
+    });
+}
